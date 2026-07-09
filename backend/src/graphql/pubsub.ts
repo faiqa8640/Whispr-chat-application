dfd58a85acd6bfca19a1da061b914ee39dff981d
faiqa20 <faiqamustafa02@gmail.com>
2026-07-09 16:19:28 +0500

    set the skeleton loader for images and voice messages

--- backend/src/graphql/pubsub.ts
+++ backend/src/graphql/pubsub.ts
@@ -1,17 +1,3 @@
-// import { PubSub } from "graphql-subscriptions";
-// export const pubsub = new PubSub();
-
-// export const EVENTS = {
-//   MESSAGE_RECEIVED: "MESSAGE_RECEIVED",
-//   MESSAGES_READ: "MESSAGES_READ",
-//   USER_UPDATED: "USER_UPDATED",
-//   MESSAGE_UNSENT: "MESSAGE_UNSENT",
-//   TYPING: "TYPING",
-//   USER_STATUS_CHANGED: "USER_STATUS_CHANGED",
-// };
-
-
-
 import { PubSub } from "graphql-subscriptions";
 export const pubsub = new PubSub();
 
